29e9f1d4601104beb57fa3d63a6ea736653d20f8
Federico Quartieri <federico.quartieri2002@gmail.com>
2021-02-26 17:44:35 +0100

    parser con i due casi

--- try.js
+++ try.js
@@ -1,14 +1,40 @@
-
 const fs=require('fs')
 const data=fs.readFileSync('input-teleportation-e539.txt',{encoding:'utf8',flag:'r'}).split('\n')
-const num = 7
+//const data=fs.readFileSync('input-antivirus-a4cc.txt',{encoding:'utf8',flag:'r'}).split('\n')
 const cases = data.shift()
+
+const rows = 0                  // variable
+const rows_before_num = 2       // variable
+
+const multiple_rows = []
 const obje = {}
-data.forEach((e,i) => {
-    
-    if(i<cases){
-    const a = data
-    obje[i+1] = a.slice(i*num,(i+1)*num)
+console.log(data)
+
+if (rows === 0) {
+    let curnum = 0
+    data.forEach((e,i) => {
+        if (i === rows_before_num + curnum ) {
+            curnum += parseInt(e) + rows_before_num + 1 
+            multiple_rows.push(curnum)
+
+        }
+    })
+    let numprev = 0
+    data.forEach((e, i) => {
+        if(i<cases){
+            num = multiple_rows[i]
+            const a = data
+            obje[i+1] = a.slice(numprev,num)
+            numprev = num
+        }
+    })
+} else {
+    data.forEach((e, i) => {
+        if(i<cases){
+            const a = data
+            obje[i+1] = a.slice(i*rows,(i+1)*rows)
+        }
+    })
 }
-})
+
 console.log(obje)
