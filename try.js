fc8f36eaaa51c828fe77e720cd49dca3cbef81da
studente <studente@Licspalla.local>
2021-02-26 12:26:37 +0100

    mods

--- try.js
+++ try.js
@@ -1,17 +1,14 @@
 
 const fs=require('fs')
-const data=fs.readFileSync('input-teleportation-e539.txt',{encoding:'utf8',flag:'r'}).split('\n').slice(1)
+const data=fs.readFileSync('input-teleportation-e539.txt',{encoding:'utf8',flag:'r'}).split('\n')
 const num = 7
-const cases=null
+const cases = data.shift()
 const obje = {}
-data.forEach((e,i
+data.forEach((e,i) => {
     
-    
-    ) => {
     if(i<cases){
     const a = data
     obje[i+1] = a.slice(i*num,(i+1)*num)
-
 }
 })
 console.log(obje)
