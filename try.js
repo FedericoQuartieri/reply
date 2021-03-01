65d49dbbfaeb5d6f7301b040cac9a7c2ed9e0d26
Federico Quartieri <federico.quartieri2002@gmail.com>
2021-03-01 11:25:23 +0100

    party

--- try.js
+++ try.js
@@ -2,61 +2,66 @@ const fs=require('fs')
 
 // parse in che restituisce oggetto (obje) con chiave caso valore dati e richiede due variabili
 
-const data=fs.readFileSync('input-teleportation-35b0.txt',{encoding:'utf8',flag:'r'}).split('\n')
+//const data=fs.readFileSync('input-teleportation-e539.txt',{encoding:'utf8',flag:'r'}).split('\n')
+//const data=fs.readFileSync('input-antivirus-a4cc.txt',{encoding:'utf8',flag:'r'}).split('\n')
+const data=fs.readFileSync('input-party-81b9.txt',{encoding:'utf8',flag:'r'}).split('\n')
+
 const cases = data.shift()
 
+const rows = 2                 // variable
+const rows_before_num = 2       // variable
 
-const parser = (rows_before_num, rows = 0) => {
-    const multiple_rows = []
-    const obje = {}
-    if (rows === 0) {
-        let curnum = 0
-        data.forEach((e,i) => {
-            if (i === rows_before_num + curnum ) {
-                curnum += parseInt(e) + rows_before_num + 1 
-                multiple_rows.push(curnum)
-    
-            }
-        })
-        let numprev = 0
-        data.forEach((e, i) => {
-            if(i<cases){
-                num = multiple_rows[i]
-                const a = data
-                obje[i+1] = a.slice(numprev,num)
-                numprev = num
-            }
-        })
-    } else {
-        data.forEach((e, i) => {
-            if(i<cases){
-                const a = data
-                obje[i+1] = a.slice(i*rows,(i+1)*rows)
-            }
-        })
-    }
-    Object.keys(obje).forEach(key => {
-        const res = obje[key].map(e=> e.split(" ").map(e => parseInt(e)))
-        obje[key] = res
+const multiple_rows = []
+const obje = {}
+
+if (rows === 0) {
+    let curnum = 0
+    data.forEach((e,i) => {
+        if (i === rows_before_num + curnum ) {
+            curnum += parseInt(e) + rows_before_num + 1 
+            multiple_rows.push(curnum)
+
+        }
     })
-    return obje
+    let numprev = 0
+    data.forEach((e, i) => {
+        if(i<cases){
+            num = multiple_rows[i]
+            const a = data
+            obje[i+1] = a.slice(numprev,num)
+            numprev = num
+        }
+    })
+} else {
+    data.forEach((e, i) => {
+        if(i<cases){
+            const a = data
+            obje[i+1] = a.slice(i*rows,(i+1)*rows)
+        }
+    })
 }
-
-const obje = parser(2) 
-
-console.log(obje)
 
 // main
 
 const result = {}
+console.log(obje)
 
 Object.keys(obje).forEach(key =>{
-    if (true){
-        console.log(key)
-        res = null
-        result[key] = res
+    const positive_array = obje[key][1].split(" ").map(e => parseInt(e)).filter(e => e > 0) 
+    console.log(positive_array)
+    let res = 0
+    if (positive_array.length === 0) {
+        res = 0
+    } else {
+        res = positive_array.reduce((total, num) => {
+            return total + num
+        })
     }
+    console.log(res)
+    result[key] = res
 })
+
+console.log(result)
 
 // parse out che richiede un oggetto con chiave caso valore risultato
 
