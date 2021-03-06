d7a346621407c18c1477100a2e3059731148d89e
Federico Quartieri <federico.quartieri2002@gmail.com>
2021-03-06 18:31:12 +0100

    riceboard fino input 3

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const lib = require("./lib");
 
 // parse in che restituisce oggetto (obje) con chiave caso valore dati
 
-const obje = lib.parser("input-server-a424.txt",{rows:2}) // input, rows:number of rows if fixed rows, exception: index of variable rows if exception, double: true if exception not single as in scoreboard
+const obje = lib.parser("input-riceboard-27ae.txt",{rows:1}) // input, rows:number of rows if fixed rows, exception: index of variable rows if exception, double: true if exception not single as in scoreboard
 
 // main
 
@@ -12,8 +12,13 @@ const result = {}
 
 Object.keys(obje).forEach(key =>{
     if (key === key){       // change key to see only one case
+        const R = BigInt(obje[key][0][0])
+        const N = BigInt((obje[key][0][1] * obje[key][0][1]) - 1)
+        const M = BigInt(obje[key][0][2])
+        const a = BigInt(R) ** BigInt(N)
+        const res = ((BigInt(2)*(BigInt(1) - a)/(BigInt(1) - R)) + BigInt(1)) % M
         console.log(key)    // must be the only console in main
-        result[key] = res = 0
+        result[key] = res
     }
 })
 
